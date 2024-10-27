0f8fe76084cd5a11a80523b876808be104318f40
Pritom003 <njahanpritom65@gmail.com>
2024-10-27 10:18:59 -0400

    set up front end

---
+++ client-site/src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div className="text-red-700 text-5xl">
+            Hello deedar
+        </div>
+    );
+};
+
+export default Home;
---
+++ client-site/src/Router.jsx
@@ -0,0 +1,21 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./Component/Layouts/Layout";
+import Home from "./Pages/Home/Home";
+import NotFound from "./Pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    errorElement:<NotFound/>,
+    children:[
+        {
+            path: "/",  
+            element: <Home />,
+          },
+    ]
+  },
+]);
+
+export default router;
