456ea48084034f32b222254e97fb241fd7a44f18
Pritom003 <njahanpritom65@gmail.com>
2024-10-29 08:03:49 -0400

    completed deedar logo

--- client-site/src/Pages/Home/MenuBox/Heading.jsx
+++ client-site/src/Pages/Home/MenuBox/Heading.jsx
@@ -4,24 +4,27 @@ import chefsticker from '../../../assets/—Pngtree—chef hat cartoon white sti
 const Heading = () => {
     return (
         <div className="relative h-20 w-44 flex items-center justify-center text-white text-2xl">
-            {/* Black Overlay */}
+            {/* Background Image */}
             <div
-                className="absolute inset-0 bg-black opacity-90" // Adjust opacity here if needed
+                className="absolute inset-0"
                 style={{
                     backgroundImage: `url(${headingbg})`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
+                    opacity: 0.9, // Adjust opacity of the background image here
                 }}
             ></div>
+            {/* Black Overlay */}
+            <div className="absolute inset-0 bg-black opacity-40"></div>
             {/* Heading Text */}
             <h2 className="font-chewy text-3xl relative z-10">DEEDAR</h2>
             {/* Chef Hat Sticker */}
             <img
                 src={chefsticker}
                 alt="Chef Hat"
-                className="absolute top-0 left-0 w-16 h-16" // Adjust size as needed
+                className="absolute top-0 left-0 w-16 opacity-90 h-16" // Adjust size as needed
                 style={{
-                    transform: 'translate(-40%, -70%) rotate(-19deg)', // Rotate the hat 15 degrees
+                    transform: 'translate(-40%, -70%) rotate(-19deg)', // Rotate the hat
                 }}
             />
         </div>
